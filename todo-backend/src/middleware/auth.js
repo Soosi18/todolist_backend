1ad9070ffd14b40566fde0502ca252ee0a42716b
Soosi18 <sarvagya.pandey2000@yahoo.com>
2024-10-17 22:14:52 +0530

    Remove Bearer token verifying method that was no longer in use

--- todo-backend/src/middleware/auth.js
+++ todo-backend/src/middleware/auth.js
@@ -1,22 +1,5 @@
 import jwt from 'jsonwebtoken'
 
-// export const verifyToken = (req, res, next) => {
-  // const bearerToken = req.headers['authorization'];
-  // const loginToken = bearerToken && bearerToken.split(' ')[1];
-  // if (!loginToken){
-    // return res.status(401).json({success: false, message: "Not authorized to access"});
-  // }
-  // jwt.verify(loginToken, process.env.SECRET_KEY, (err, user) => {
-    // if (err){
-      // res.json({success: false, message: "Failed to Authenticate"});
-    // }
-    // else{
-      // req.user = user;
-      // next();
-    // }
-  // })
-//}
-
 export const verifyToken = (req, res, next) => {
   const authCookie = req.cookies['auth'];
   if (!authCookie){
@@ -24,7 +7,7 @@ export const verifyToken = (req, res, next) => {
   } 
   jwt.verify(authCookie, process.env.SECRET_KEY, (err, user) => {
     if (err){
-      res.json({success: false, message: "User Authentication Failed"});
+      res.status(401).json({success: false, message: "User Authentication Failed"});
     }
     else{
       req.user = user;
