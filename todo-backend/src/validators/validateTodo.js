3fb21148140050a782855362837d789204b9447f
Soosi18 <sarvagya.pandey2000@yahoo.com>
2024-10-17 22:15:21 +0530

    Add validation rule for list names

--- todo-backend/src/validators/validateTodo.js
+++ todo-backend/src/validators/validateTodo.js
@@ -5,10 +5,15 @@ export const validateTodo = [
     .isLength({min: 1, max: 255}).withMessage("Task character count must be between 1 and 255"),
   body("isComplete").trim().toLowerCase()
     .notEmpty().withMessage("Value cannot be empty")
-    .isBoolean({loose: false}).withMessage("Value must be true, false, 0, 1, yes or no")
+    .isBoolean({loose: false}).withMessage("Value must be true")
 ]
 
 export const validateId = [
   param("id").trim()
     .isInt({min: 1}).withMessage("ID must be integer starting from 1")
+]
+
+export const validateListName = [
+  body("name").trim().escape()
+    .isLength({min: 1, max: 32}).withMessage("List name must be between 1 and 32 characters")
 ]
