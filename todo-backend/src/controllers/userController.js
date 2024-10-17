6bded17e0e85fac7486f3a5484a47f2978f7b3c7
Soosi18 <sarvagya.pandey2000@yahoo.com>
2024-10-17 22:13:59 +0530

    Provide more meaningful status code on failed registering and add method to get current user

--- todo-backend/src/controllers/userController.js
+++ todo-backend/src/controllers/userController.js
@@ -4,7 +4,7 @@ import asyncHandler from 'express-async-handler';
 import jwt from 'jsonwebtoken';
 
 export const loginUser = async(req, res) => {
-  const failMessage = "Failed to login due to incorrect credentials...";
+  const failMessage = "Failed to login due to incorrect credentials";
   const foundUser = await db.getUser(req.body.username);
 
   if (foundUser){
@@ -15,24 +15,34 @@ export const loginUser = async(req, res) => {
         username: foundUser.username
       }, process.env.SECRET_KEY);
       res.cookie('auth', loginToken, {httpOnly: true, maxAge: 1000 * 60 * 30});
-      res.json({success: true, message: "Successfully logged in!"});
+      res.json({success: true, username: req.body.username, message: "Successfully logged in!"});
     }
     else
       res.status(401).json({success: false, message: failMessage});
   }
   else{
-    return res.status(401).json({success: false, message: failMessage});
+    res.status(401).json({success: false, message: failMessage});
   }
 }
 
 export const registerUser = asyncHandler(async(req, res) => {
   const username = req.body.username;
-  const password = await bcrypt.hash(req.body.password, 10);
-  await db.addNewUser(username, password);
-  res.json({success: true, message: "Successfully created new user!"});
+  const foundUser = await db.getUser(username);
+  if(!foundUser){
+    const password = await bcrypt.hash(req.body.password, 10);
+    await db.addNewUser(username, password);
+    res.json({success: true, message: "Successfully registered!"});
+  }
+  else {
+    res.status(409).json({success: false, message: "Username already taken"});
+  }
 });
 
 export const logoutUser = async(req, res) => {
   res.clearCookie("auth");
-  res.json({success: true, message: "Successfully logged out"});
+  res.json({success: true, message: "Successfully logged out!"});
+}
+
+export const getCurrentUser = async(req, res) => {
+  res.json({success: true, username: req.user.username});
 }
