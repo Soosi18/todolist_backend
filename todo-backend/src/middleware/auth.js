8fbd9faf2bf8ef77391c504be8ec4533e680d50a
Soosi18 <sarvagya.pandey2000@yahoo.com>
2025-01-22 10:30:34 +0530

    Add separate messages for auth failure and not logged in status

--- todo-backend/src/middleware/auth.js
+++ todo-backend/src/middleware/auth.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken'
 export const verifyToken = (req, res, next) => {
   const authCookie = req.cookies['auth'];
   if (!authCookie){
-    return res.status(401).json({success: false, message: "User Authentication Failed"});
+    return res.status(401).json({success: false, message: "User Not Logged In"});
   } 
   jwt.verify(authCookie, process.env.SECRET_KEY, (err, user) => {
     if (err){
